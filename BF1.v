22e270c90eed34250596b0976af752d7d6729fb0
Luis-MGR <lumanuelgonzalez@gotmail.com>
2020-05-03 18:18:02 -0500

    BF1

--- BF1.v
+++ BF1.v
@@ -16,8 +16,8 @@ module BF1(
 // Cuerpo del modulo
 always@(posedge clk_BF1)
 begin
-   data1BF1 <= dIn1_BF1;
-   data2BF1 <= dIn2_BF1;
+   data1BF1 <= dIn1_BF1; //Van al MUX1
+   data2BF1 <= dIn2_BF1; //Van al MUX1
    functBF1 <= dIn3_BF1; // Funct con bits de SignE
    rdBF1 <= dIn4_BF1; // rd van al MUX2
    rtBF1 <= dIn5_BF1; // rt van al MUX2
