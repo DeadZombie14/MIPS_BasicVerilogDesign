23e692dd17c74e3fc3bf813377216f4abf087fcb
Luis-MGR <lumanuelgonzalez@gotmail.com>
2020-05-03 18:09:31 -0500

    BF1

--- BF1.v
+++ BF1.v
@@ -7,7 +7,7 @@ module BF1(
     input clk_BF1,
 	output reg [31:0]data1BF1,data2BF1,functBF1,
     output reg [4:0]rdBF1,
-    output reg [4:0]shamtBF1
+    output reg [4:0]rtBF1,
     output reg [7:0]nextIns_BF1
 );
 // Conexiones 
@@ -19,8 +19,8 @@ begin
    data1BF1 <= dIn1_BF1;
    data2BF1 <= dIn2_BF1;
    functBF1 <= dIn3_BF1; // Funct con bits de SignE
-   rdBF1 <= dIn4_BF1; // rd
-   shamtBF1 <= dIn5_BF1; // shamt
+   rdBF1 <= dIn4_BF1; // rd van al MUX2
+   rtBF1 <= dIn5_BF1; // rt van al MUX2
    nextIns_BF1 <= dIn6_BF1; //Instruccion de adder
 end
 
