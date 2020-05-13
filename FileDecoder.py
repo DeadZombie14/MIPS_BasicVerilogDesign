7c09c7dc466cd295fd3e385da9b3313e7c080d5d
DeadZombie14 <mathiermenmore@gmail.com>
2020-05-13 01:25:38 -0500

    Multiples cambios en decodificador GUI
    - Añadido datos de autor, para evitar que se piense que robé código o algo así. Aún así prefiero mantener el programa en inglés.
    - Ya no existen los botones con texto.
    - Eliminado archivo de ícono, para evitar subir mas archivos innecesarios al proyecto trataré de no poner imágenes.
    - Ahora hay dos notepad editors: una a la izquierda para código ensamblador y
    otro a la derecha para código máquina.
    - Ahora se cargan archivos desde botones con "..."
    - Los botones con "=>" y "<=" son para traducir en ambos sentidos.
    - De momento, no hay implementación del decodificador en si.

--- FileDecoder.py
+++ FileDecoder.py
@@ -1,11 +1,11 @@
 # Decoder for MIPS32 Basic Verilog Implementation
+# Made by DeadZombie14 (Mendoza Morelos Martin Mathier)
 # Basically, translates MIPS32 assembly code to its binary representation format.
 # Uses library tkinter as GUI library.
 
-# Main Icon made by Nhor Phai (https://www.flaticon.com/authors/nhor-phai)
 from tkinter import *
 from tkinter import filedialog
-import tkinter.messagebox
+from tkinter import messagebox
 
 class FileDecoder:
     def __init__(self):
@@ -17,12 +17,14 @@ class FileDecoder:
     def createScreen(self):
         self.mainScreen = Tk()
         self.mainScreen.title('MIPS32 Assembly Decoder')
-        self.mainScreen.iconbitmap(r'src/icon.ico')
-        self.mainScreen.geometry('800x600')
+        #self.mainScreen.iconbitmap(r'src/icon.ico')
+        self.width = 800
+        self.height = 600
+        self.mainScreen.geometry(str(self.width)+"x"+str(self.height))
 
         # Create menu toolbar
         menubar = Menu(self.mainScreen)
-        self.mainScreen.config(menu=menubar)
+        self.mainScreen.config(menu=menubar,bg="gray")
         
         # Entry 1: File manager
         fileMButton = Menu(menubar, tearoff=0) # Tearoff removes dashed lines
@@ -34,29 +36,93 @@ class FileDecoder:
         # Entry 2: Help
         helpMButton = Menu(menubar, tearoff=0)
         menubar.add_cascade(label='Help',menu=helpMButton)
-        helpMButton.add_command(label="About",command=lambda: tkinter.messagebox.showinfo('About','MIPS32 Decoder, 2020'))
+        helpMButton.add_command(label="About",command=lambda: messagebox.showinfo('About','MIPS32 Decoder, 2020'))
 
-        text = Label(self.mainScreen, text = 'Lets go!')
-        text.pack()
+        editors = Frame(self.mainScreen)
+        editors.pack()
+        
+        # Assembly editor
+        leftEditor = Frame(editors)
+        leftEditor.pack(side=LEFT,fill=BOTH,padx=5, pady=5, expand=True)
 
-        loadFile = Button(self.mainScreen,text='Load assembly file',command=lambda: self.openFile())
-        loadFile.pack()
+        codeEditorLabel = Label(leftEditor,text="Assembly code editor")
+        codeEditorLabel.pack(side=TOP)
 
-        saveFile = Button(self.mainScreen,text='Save memory file',command=lambda: print('save'))
-        saveFile.pack()
+        codeDirectory = Frame(leftEditor)
+        codeDirectory.pack(fill=X)
+        self.codeDirectoryVar = StringVar()
+        inputCD = Entry(codeDirectory,width=52,textvariable=self.codeDirectoryVar,bg="white",fg="black")
+        inputCD.pack(side=LEFT)
+        buttonCD = Button(codeDirectory,text='...',command=lambda: self.openCodeFile())
+        buttonCD.pack(side=RIGHT)
 
-        self.filePreview = Label(self.mainScreen, bg="white", width=64,height=30)
-        self.filePreview.pack()
+        scrollbarCPx = Scrollbar(leftEditor, orient=HORIZONTAL)
+        scrollbarCPx.pack(side=BOTTOM,fill=X)
+        scrollbarCPy = Scrollbar(leftEditor, orient=VERTICAL)
+        scrollbarCPy.pack(side=RIGHT,fill=Y)
+        self.codePreview = Text(leftEditor,bg="white",width=40,fg="black",xscrollcommand=scrollbarCPx.set,yscrollcommand=scrollbarCPy.set,wrap=NONE) 
+        self.codePreview.pack(fill=Y)
+        scrollbarCPx.config(command=self.codePreview.xview)
+        scrollbarCPy.config(command=self.codePreview.yview)
+
+        # Switching arrows
+        switchingArrows = Frame(editors)
+        switchingArrows.pack(side=LEFT)
+        toBinaryButton = Button(switchingArrows,text="=>")
+        toBinaryButton.pack(side=TOP)
+        toAssemblyButton = Button(switchingArrows,text="<=")
+        toAssemblyButton.pack(side=TOP)
+
+
+        # Binary editor
+        rightEditor = Frame(editors)
+        rightEditor.pack(side=RIGHT,fill=BOTH,padx=5, pady=5, expand=True)
+
+        binaryEditorLabel = Label(rightEditor,text="Binary editor")
+        binaryEditorLabel.pack(side=TOP)
+
+        binaryDirectory = Frame(rightEditor)
+        binaryDirectory.pack(fill=X)
+        self.binaryDirectoryVar = StringVar()
+        inputBD = Entry(binaryDirectory,textvariable=self.binaryDirectoryVar,width=52,bg="white",fg="black")
+        inputBD.pack(side=LEFT)
+        buttonBD = Button(binaryDirectory,text='...',command=lambda: self.openBinaryFile())
+        buttonBD.pack(side=RIGHT)
+
+        scrollbarBPx = Scrollbar(rightEditor, orient=HORIZONTAL)
+        scrollbarBPx.pack(side=BOTTOM,fill=X)
+        scrollbarBPy = Scrollbar(rightEditor, orient=VERTICAL)
+        scrollbarBPy.pack(side=RIGHT,fill=Y)
+        self.binaryPreview = Text(rightEditor,bg="white",width=40,fg="black",xscrollcommand=scrollbarBPx.set,yscrollcommand=scrollbarBPy.set,wrap=NONE)
+        self.binaryPreview.pack(fill=Y)
+        scrollbarBPx.config(command=self.binaryPreview.xview)
+        scrollbarBPy.config(command=self.binaryPreview.yview)
+
+
+        statusBar = Label(self.mainScreen, text = 'Please load a MIPS assembly file.',relief=RAISED,anchor=W)
+        statusBar.pack(side=BOTTOM,fill=X)
 
         pass
 
-    def openFile(self):
+    def openCodeFile(self):
         # Open file browser
         assemblyFile = filedialog.askopenfilename()
         assemblyCode = open(assemblyFile, "r")
         print(assemblyFile)
-        print(assemblyCode.readlines())
-        self.filePreview.insert('hola')
+        self.codeDirectoryVar.set(str(assemblyFile))
+        self.codePreview.delete(1.0,END)
+        self.codePreview.insert(1.0,assemblyCode.read())
+        assemblyCode.close()
+        pass
+    
+    def openBinaryFile(self):
+        # Open file browser
+        assemblyFile = filedialog.askopenfilename()
+        assemblyCode = open(assemblyFile, "r")
+        print(assemblyFile)
+        self.binaryDirectoryVar.set(str(assemblyFile))
+        self.binaryPreview.delete(1.0,END)
+        self.binaryPreview.insert(1.0,assemblyCode.read())
         assemblyCode.close()
         pass
 
