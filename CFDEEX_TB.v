5084a80ae7ef6eb1fefd359dd6a2547550ba21bf
alejandraroobles3 <64538989+alejandraroobles3@users.noreply.github.com>
2020-05-14 19:24:39 -0500

    Revert "."
    This reverts commit a7cfc48254b1a677f0c8a8c948e5788b014b1224.

---
+++ CFDEEX_TB.v
@@ -0,0 +1,38 @@
+// `timescale 1ns/1ps // Definir escala de tiempo
+// module CFDEEX_TB();
+
+// === Señales ===
+// hola 
+//Registros
+reg clk_DETB,regWriteFlagDETB;
+//Conexiones
+wire [31:0]res_DETB;
+wire [5:0]op_DETB;
+wire zf_DETB;
+//Instancia
+CFDEEX cf_de_ex(.initPCvalue(8'b00000000),.clk_DE(clk_DETB),.regWriteFlagDE(regWriteFlagDETB),.res_DE(res_DETB),.op_DE(op_DETB),.zf_DE(zf_DETB));
+always #10 clk_DETB = !clk_DETB;
+
+// //Registros
+// reg clk_DETB,regWriteFlagDETB;
+// //Conexiones
+// wire [31:0]res_DETB;
+// wire [5:0]op_DETB;
+// wire zf_DETB;
+// //Instancia
+// CFDEEX cf_de_ex(.initPCvalue(8'b00000000),.clk_DE(clk_DETB),.regWriteFlagDE(regWriteFlagDETB),.res_DE(res_DETB),.op_DE(op_DETB),.zf_DE(zf_DETB));
+// always #10 clk_DETB = !clk_DETB;
+
+// initial
+// 	begin
+//     clk_DETB = 1'b0;
+// 	regWriteFlagDETB = 1'b1;
+// 	$readmemb("D:/intelFPGA/CF-DE-EX/BancoRegistrosMem.bin", cf_de_ex.BRDE.registerBank);
+// 	$readmemb("D:/intelFPGA/CF-DE-EX/instruccionesBinario.bin", cf_de_ex.IMDE.instBank);
+// 	#30 // Dar tiempo a cargar la memoria.
+
+//     #400
+    
+// 	$stop;
+// 	end
+// endmodule
